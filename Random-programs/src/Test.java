a225b487d758f2885098ae5d284db40efd2b8b11
adithya141 <krishnaaditya141@gmail.com>
2016-10-21 19:01:01 -0600

    new changes

---
+++ Random-programs/src/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Owner on 10/21/2016.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
--- Random-programs/src/first.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Owner on 10/19/2016.
- */
-public class first {
-
-}
